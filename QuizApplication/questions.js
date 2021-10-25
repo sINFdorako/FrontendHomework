2c9d9ec2c9aef8b81aca2b0a29c37dc3164fc69d
Dominik Rakowski <dominik@MacBook-Air-von-Dominik.local>
2021-10-25 16:08:31 +0200

    created dummy question objects

---
+++ QuizApplication/questions.js
@@ -0,0 +1,21 @@
+export const questions = {
+    question: {
+        question: 'Some Question', correctAnswer: 'a',
+        a: 'answerA', b: 'answerB', c: 'answerC', d: 'answerD'
+    },
+    question: {
+        question: 'Some Question', correctAnswer: 'a',
+        a: 'answerA', b: 'answerB', c: 'answerC', d: 'answerD'
+    },
+    question: {
+        question: 'Some Question', correctAnswer: 'a',
+        a: 'answerA', b: 'answerB', c: 'answerC', d: 'answerD'
+    },
+    question: {
+        question: 'Some Question', correctAnswer: 'a',
+        a: 'answerA', b: 'answerB', c: 'answerC', d: 'answerD'
+    }
+}
+
+
+
